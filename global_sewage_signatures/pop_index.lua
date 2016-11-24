bbe9493acf80077db5596ce123ab5e2d28f3ca8b
Jose L. Bellod Cisneros <bellod.cisneros@gmail.com>
2016-11-24 10:01:28 +0100

    Pop Index Lua finished. FIXME. Table overflow

--- global_sewage_signatures/pop_index.lua
+++ global_sewage_signatures/pop_index.lua
@@ -3,7 +3,7 @@ local index = 0
 local start_byte = 0
 local end_byte = -1
 local index_table = {}
-local total_count = 0
+local total_count = 1
 local value = redis.call('BITPOS', KEYS[1], 1, start_byte, end_byte)
 
 -- We start looping over bytes
@@ -13,12 +13,13 @@ while value ~= -1 do
     total_count = total_count + 1
     local other_ones = 0
     -- Inspecting all other bits in byte
-    for i=first_one,7 do
+    for i=first_one + 1,7 do
         other_ones = redis.call('BITFIELD', KEYS[1], 'GET', 'u1', i)
-        if other_ones == 1 then
+        if other_ones[1] == 1 then
             index_table[total_count] = start_byte * 8 + i
             total_count = total_count + 1
         end
+        -- index_table[i] = other_ones
     end
     start_byte = start_byte + 1
     value = redis.call('BITPOS', KEYS[1], 1, start_byte, end_byte)
--- global_sewage_signatures/sewage.py
+++ global_sewage_signatures/sewage.py
@@ -12,30 +12,30 @@
 from global_sewage_signatures import MinHash as MH
 import redis
 from pkg_resources import resource_filename
+import numpy as np
+
 
 class MinHashRedis(MH.MinHash):
 
-    def __init__(self, dist, hash_funcs, documents, features, redis_db):
-        super(self.__class__, self).__init__(dist, [], k_permutations)
+    def __init__(self, dist, h_funcs, docs, features, redis_db, module, lua_f):
+        # Dummy sparse set so MinHash won't complain
+        empty_sparse = np.matrix('0; 0')
+        super(self.__class__, self).__init__(dist, empty_sparse, h_funcs)
         self.dimensions = features
-        self.n_points = documents
+        self.n_points = docs
         self.redis = redis_db
-
-    # def redis_key_indices(self, key):
-    #     indices = []
-    #     start = 0
-    #     end = 0
-    #     while (zeroes=(redis_db.bitpos(key, 0, start, end) != -1)):
-    #         # Append indices of ones (all before the first zero)
-    #         indices.append(range(start, zeroes - 1))
-    #         # Update start/end
-    #         start = zeroes
+        with open(resource_filename(module, lua_f)) as lua_file:
+            # Load Lua script as a string
+            lua = lua_file.read()
+            # Lua script now can be called as a function called pop_index(KEY)
+            self.pop_index = self.redis.register_script(lua)
 
     def createMinHash(self):
         for col_j in range(0, self.n_points):
+            print('Pushing document... ', col_j)
             for sign_i, hash_func in enumerate(self.hash_permutations):
-                for row_i in self.redis_key_indices(col_j):
-                # for row_i in self.sparse_matrix[col_j].indices:
+                for row_i in self.pop_index(keys=[col_j]):
+                    print(row_i)
                     hash_row = hash_func(row_i)
                     if hash_row < self.signatures[col_j][sign_i]:
                         self.signatures[col_j][sign_i] = hash_row
@@ -43,34 +43,24 @@ class MinHashRedis(MH.MinHash):
 # Hostname comes from docker-compose.yml "depends_on" directive
 redis_db = redis.StrictRedis(db=0, host='redis', port=6379)
 
-redis_db_test = redis.StrictRedis(db=3, host='redis', port=6379)
-
-# Extract keys from redis db and create MinHash using Lua scripting
-# lua = """
-#     local index = 0
-#
-#
-#     local value = redis.call('BITFIELD', KEYS[1], 'GET', 'u1', index)
-#      BITFIELD test GET u1 0
-#     for bit in
-#     value = tonumber(value)
-#     return value * ARGV[1]
-# """
-
-with open(resource_filename('global_sewage_signatures', 'pop_index.lua')) as lua_f:
-    lua = lua_f.read()
-    pop_index = redis_db_test.register_script(lua)
-
-
-print(pop_index(keys=['test']))
-
-# Dataset contains 85 x 4^16 datapoints.
-#
-# MinHash class expects a sparse_matrix containing indexes for each of the documents
-#
-
-distance = 0.2
+dist = 0.2
 sparse_matrix = []
-hash_functions = 100
+hash_funcs = 100
+keys = len(redis_db.keys())
+module = 'global_sewage_signatures'
+lua_file = 'pop_index.lua'
 
-# mh = Mh.MinHash(dist, sparse_matrix, k_permutations)
+with open(resource_filename(module, lua_file)) as lua_file:
+    # Load Lua script as a string
+    lua = lua_file.read()
+    # Lua script now can be called as a function called pop_index(KEY)
+    pop_index = redis_db.register_script(lua)
+    pop_index(keys=[0])
+# Dataset contains 85 x 4^16 datapoints.
+# mh = MinHashRedis(dist, hash_funcs, keys, 4**16, redis_db, module, lua_file)
+# mh.createMinHash()
+
+# Save signature matrix to Redis, DB[4]
+# redis_db_signs = redis.StrictRedis(db=4, host='redis', port=6379)
+# for row, signatures in enumerate(mh.signatures):
+#     redis_db_signs.set(row, signatures)
